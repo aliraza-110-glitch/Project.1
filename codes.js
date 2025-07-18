89a629a41413c8cb4d418871cd1afc08751aec87
Ali Raza <syedmuhammadalirazaa@gmail.com>
2025-07-18 10:14:14 +0500

    Codes

---
+++ codes.js
@@ -0,0 +1,50 @@
+//Declare and Log a String Variable
+//var MyName = 'AliRaza'
+//console.log(MyName)
+
+//Store Your Age in a Number Variable
+//const age=22
+//console.log(age)
+
+//Boolean Example
+//let IsRaining = 'True'
+//console.log(IsRaining)
+
+//Check Undefined
+//var Person;
+//console.log(Person)
+
+//Null Example
+//let num = null
+//console.log(num)
+
+//Use of var, let, and const
+//var Country = ['Pakistan', 'India', 'Australia', 'England']
+//let city = ['Lahore', 'NewDehli', 'Melbourne', 'Wales']
+//const num = 22
+//console.log(Country, city, num)
+
+//Array of Fruits
+//var fruits = ['Banana', 'Apples', 'Grapes']
+//console.log(fruits[1])
+
+//Create an Object
+//var Student = {
+//    name: 'Ali',
+//    age: 22
+//}
+//console.log(Student)
+
+//Update a Variable
+//let num = 20;
+//num = 30
+//console.log(num)
+
+//Type Checking
+let value = 'Hassan';
+let num = 21;
+let flag = 'true'
+
+console.log(value)
+console.log(num)
+console.log(flag)
