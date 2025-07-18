747805389bc3af6348e6f5cd120833f10e80ea96
Ali Raza <syedmuhammadalirazaa@gmail.com>
2025-07-18 14:34:25 +0500

    Function

--- functions.js
+++ functions.js
@@ -1,10 +1,13 @@
-//greet();
-//function greet(){
-c//onsole.log("SayHello!");
-//}
+function AreaOfRectangle(length, width) {
+    return length * width
+}
 
 
-const sayhi = function() {
-    console.log("Hello");
-};
-sayhi();s
+
+const length = AreaOfRectangle(12, 12);
+const width = AreaOfRectangle(11,11);
+const result = AreaOfRectangle(length, width)
+
+console.log(length)
+console.log(width)
+console.log(result)
